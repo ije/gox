5812dc5f418d4bca35c45fe7128e7b6f17235d10
Je <git@iamje.com>
2015-07-08 22:22:19 +0800

    add IsDomain & IsIP functions

--- valid/valid.go
+++ valid/valid.go
@@ -2,34 +2,16 @@ package valid
 
 import (
 	"github.com/ije/gox/utils"
+	"strings"
 )
 
 var (
 	emailNameValidator = &Validator{[]Range{{'0', '9'}, {'a', 'z'}, {'A', 'Z'}, {'.', 0}, {'-', 0}, {'_', 0}}}
-	emailDNValidator   = &Validator{[]Range{{'0', '9'}, {'a', 'z'}, {'A', 'Z'}, {'.', 0}, {'-', 0}}}
-	emailDTValidator   = &Validator{[]Range{{'a', 'z'}, {'A', 'Z'}}}
+	DNValidator        = &Validator{[]Range{{'0', '9'}, {'a', 'z'}, {'A', 'Z'}, {'.', 0}, {'-', 0}}}
+	DTValidator        = &Validator{[]Range{{'a', 'z'}, {'A', 'Z'}}}
 	numberValidator    = &Validator{[]Range{{'0', '9'}}}
 	hexValidator       = &Validator{[]Range{{'0', '9'}, {'a', 'f'}, {'A', 'F'}}}
 )
-
-func IsEmail(s string) bool {
-	name, domain := utils.SplitByLastByte(s, '@')
-	dn, dt := utils.SplitByLastByte(domain, '.')
-	nl, dl, tl := len(name), len(dn), len(dt)
-	if nl*dl*tl == 0 {
-		return false
-	}
-	for _, c := range []byte{name[0], name[nl-1], dn[0], dn[dl-1]} {
-		switch c {
-		case '.', '_', '-':
-			return false
-		}
-	}
-	if !emailNameValidator.Is(name) || !emailDNValidator.Is(dn) || !emailDTValidator.Is(dt) {
-		return false
-	}
-	return true
-}
 
 func IsNumber(s string, n ...int) bool {
 	return numberValidator.Is(s, n...)
@@ -37,4 +19,58 @@ func IsNumber(s string, n ...int) bool {
 
 func IsHexString(s string, n ...int) bool {
 	return hexValidator.Is(s, n...)
+}
+
+func IsIP(s string) bool {
+	for i, p := range strings.Split(s, ".") {
+		if i > 3 || !numberValidator.Is(p, 1, 3) || p[0] > '2' {
+			return false
+		}
+	}
+	return true
+}
+
+func IsDomain(s string) bool {
+	if len(s) < 4 {
+		return false
+	}
+	dn, dt := utils.SplitByLastByte(s, '.')
+	dl, tl := len(dn), len(dt)
+	if dl*tl == 0 {
+		return false
+	}
+	for _, c := range []byte{dn[0], dn[dl-1]} {
+		switch c {
+		case '.', '-':
+			return false
+		}
+	}
+	if !DNValidator.Is(dn) || !DTValidator.Is(dt) {
+		return false
+	}
+	return true
+}
+
+func IsEmail(s string) bool {
+	if len(s) < 6 {
+		return false
+	}
+	name, domain := utils.SplitByLastByte(s, '@')
+	if !IsDomain(domain) {
+		return false
+	}
+	nl := len(name)
+	if nl == 0 {
+		return false
+	}
+	for _, c := range []byte{name[0], name[nl-1]} {
+		switch c {
+		case '.', '_', '-':
+			return false
+		}
+	}
+	if !emailNameValidator.Is(name) {
+		return false
+	}
+	return true
 }
