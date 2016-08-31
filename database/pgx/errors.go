70ca20808b3c6a72715aad2051bacdf14df926b6
Je <git@iamje.com>
2016-08-31 13:17:23 +0800

    add IsDBError for pgx package

--- database/pgx/errors.go
+++ database/pgx/errors.go
@@ -7,6 +7,11 @@ import (
 
 var ErrNoRows = pgx.ErrNoRows
 
+func IsDBError(err error) bool {
+	_, ok := err.(pgx.PgError)
+	return ok
+}
+
 func IsDupError(err error, constraintName string) bool {
 	pgErr, ok := err.(pgx.PgError)
 	return ok && pgErr.Code == "23505" && (pgErr.ConstraintName == constraintName || strings.Contains(pgErr.ConstraintName, constraintName))
