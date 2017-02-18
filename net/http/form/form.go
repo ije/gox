89f20bb2a3d465c39770a944ff9d1cb586afb6a5
Je <git@iamje.com>
2017-02-18 13:01:49 +0800

    rewrite form package

--- net/http/form/form.go
+++ net/http/form/form.go
@@ -6,72 +6,97 @@ import (
 	"io"
 	"mime/multipart"
 	"net/http"
+	URL "net/url"
 	"os"
 	"path"
+	"strings"
 )
 
-func Upload(method string, url string, filepath string, fileFieldName string, extraHeader map[string]string) (ret []byte, err error) {
-	// caveat IMO dont use this for large files, create a tmpfile and assemble your multipart from there (not tested)
-	buf := bytes.NewBuffer(nil)
-	w := multipart.NewWriter(buf)
+func Form(url string, method string, params map[string]string, files map[string]string) (resp *http.Response, err error) {
+	var buf *bytes.Buffer
+	var contentType string
+	var contentSize int
 
-	// Create file field
-	fw, err := w.CreateFormFile(fileFieldName, path.Base(filepath))
-	if err != nil {
-		return
-	}
+	if len(files) == 0 {
+		if len(params) > 0 {
+			values := URL.Values{}
+			for key, value := range params {
+				values.Add(key, value)
+			}
+			if strings.ContainsRune(url, '?') {
+				url += "&"
+			} else {
+				url += "?"
+			}
 
-	fd, err := os.Open(filepath)
-	if err != nil {
-		return
-	}
-	defer fd.Close()
-
-	// Write file field from file to upload
-	_, err = io.Copy(fw, fd)
-	if err != nil {
-		return
-	}
-
-	for key, val := range extraHeader {
-		var fw io.Writer
-		fw, err = w.CreateFormField(key)
-		if err != nil {
+			if method == "GET" || method == "HEAD" || method == "DELETE" {
+				url += values.Encode()
+			} else {
+				buf = bytes.NewBufferString(values.Encode())
+				contentSize = buf.Len()
+				contentType = "application/x-www-form-urlencoded"
+			}
+		}
+	} else {
+		if method == "GET" || method == "HEAD" || method == "DELETE" {
+			err = errors.New("Invalid Method")
 			return
 		}
-		_, err = fw.Write([]byte(val))
-		if err != nil {
-			return
-		}
-	}
 
-	// Important if you do not close the multipart writer you will not have a terminating boundry
-	w.Close()
+		// caveat IMO dont use this for large files, create a tmpfile and assemble your multipart from there (not tested)
+		buf = bytes.NewBuffer(nil)
+		w := multipart.NewWriter(buf)
+
+		for fieldName, filePath := range files {
+			var fw io.Writer
+			var fd *os.File
+
+			fw, err = w.CreateFormFile(fieldName, path.Base(filePath))
+			if err != nil {
+				return
+			}
+
+			fd, err = os.Open(filePath)
+			if err != nil {
+				return
+			}
+
+			_, err = io.Copy(fw, fd)
+			fd.Close()
+			if err != nil {
+				return
+			}
+		}
+
+		for key, val := range params {
+			var fw io.Writer
+			fw, err = w.CreateFormField(key)
+			if err != nil {
+				return
+			}
+			_, err = fw.Write([]byte(val))
+			if err != nil {
+				return
+			}
+		}
+
+		// Important if you do not close the multipart writer you will not have a terminating boundry
+		w.Close()
+		contentType = w.FormDataContentType()
+	}
 
 	req, err := http.NewRequest(method, url, buf)
 	if err != nil {
 		return
 	}
-	req.Header.Set("Content-Type", w.FormDataContentType())
+	if len(contentType) > 0 {
+		req.Header.Set("Content-Type", contentType)
+	}
+	if contentSize > 0 {
+		req.ContentLength = int64(contentSize)
+	}
 
 	var client http.Client
-	res, err := client.Do(req)
-	if err != nil {
-		return
-	}
-	defer res.Body.Close()
-
-	if res.StatusCode >= 300 {
-		err = errors.New(res.Status)
-		return
-	}
-
-	buf = bytes.NewBuffer(nil)
-	_, err = io.Copy(buf, res.Body)
-	if err != nil {
-		return
-	}
-
-	ret = buf.Bytes()
+	resp, err = client.Do(req)
 	return
 }
--- net/http/form/form_test.go
+++ net/http/form/form_test.go
@@ -10,11 +10,14 @@ import (
 func Test(t *testing.T) {
 	tmp := path.Join(os.TempDir(), "hello.txt")
 	defer os.Remove(tmp)
+
 	ioutil.WriteFile(tmp, []byte("Hello World!"), 0644)
-	ret, err := Upload("PUT", "http://x-stud.io/api/file", tmp, "file", map[string]string{
+	ret, err := Form("http://x-stud.io/api/file", "PUT", map[string]string{
 		"saveas":   "test/hello.txt",
 		"email":    "",
 		"password": "",
+	}, map[string]string{
+		"file": tmp,
 	})
 	t.Log(string(ret), err)
 }
