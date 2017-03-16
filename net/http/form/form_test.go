78849fdddd5e793d122774e8ace658cde89696b8
Je <git@iamje.com>
2017-03-16 11:09:49 +0800

    improve testing

--- net/http/form/form_test.go
+++ net/http/form/form_test.go
@@ -1,23 +1,46 @@
 package form
 
 import (
+	"encoding/base64"
 	"io/ioutil"
+	"net/http"
 	"os"
 	"path"
 	"testing"
+	"time"
 )
 
 func Test(t *testing.T) {
 	tmp := path.Join(os.TempDir(), "hello.txt")
+	ioutil.WriteFile(tmp, []byte("Hello World!"), 0644)
 	defer os.Remove(tmp)
 
-	ioutil.WriteFile(tmp, []byte("Hello World!"), 0644)
-	ret, err := Form("http://x-stud.io/api/file", "PUT", map[string]string{
-		"saveas":   "test/hello.txt",
-		"email":    "",
-		"password": "",
+	go func() {
+		http.ListenAndServe(":8080", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			t.Log("Authorization(header):", r.Header.Get("Authorization"))
+			t.Log("key:", r.FormValue("key"))
+			_, h, err := r.FormFile("file")
+			if err != nil {
+				w.WriteHeader(500)
+			}
+			t.Log("file:", h)
+			w.WriteHeader(200)
+		}))
+	}()
+	time.Sleep(time.Second)
+
+	ret, err := Form("http://127.0.0.1:8080", "POST", map[string]string{
+		"Authorization": "Basic " + base64.StdEncoding.EncodeToString([]byte("user:password")),
+	}, map[string]string{
+		"key": "value",
 	}, map[string]string{
 		"file": tmp,
 	})
-	t.Log(string(ret), err)
+	if err != nil {
+		t.Fatal(err)
+		return
+	}
+
+	data, _ := ioutil.ReadAll(ret.Body)
+	t.Log(ret, string(data))
 }
