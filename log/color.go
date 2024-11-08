9911e32adea74528ff3e9e64918f9a3ac0e97922
Je Xia <i@jex.me>
2024-11-08 17:34:52 +0800

    Add 'cyan' color

--- log/color.go
+++ log/color.go
@@ -4,6 +4,7 @@ var (
 	reset  = "\033[0m"
 	grey   = "\033[90m"
 	green  = "\033[32m"
+	cyan   = "\033[36m"
 	yellow = "\033[33m"
 	red    = "\033[31m"
 )
@@ -21,6 +22,11 @@ func Grey(msg string) string {
 // Green returns a green colored string for terminal output.
 func Green(msg string) string {
 	return colorize(msg, green)
+}
+
+// Cyan returns a cyan colored string for terminal output.
+func Cyan(msg string) string {
+	return colorize(msg, cyan)
 }
 
 // Yellow returns a yellow colored string for terminal output.
